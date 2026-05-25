2ccf8b24fe1ce0dd1d4a7a17c151c37f9b64a6c2
Richard <152018239+RichardWASD@users.noreply.github.com>
2026-05-25 17:29:08 -0400

    Add: is-anagram - submission-2

---
+++ Data Structures & Algorithms/is-anagram/submission-2.py
@@ -0,0 +1,27 @@
+class Solution:
+    '''
+    U:
+        I: 2 strings
+        O: bool
+        C:
+        E: different amount of letters
+    '''
+    def isAnagram(self, s: str, t: str) -> bool:
+        Ls = len(s);
+        Lt = len(t);
+        if(not s or not t or Ls != Lt):
+            return False;
+        
+        book1 = {};
+        book2 = {};
+        for i in range (Ls):
+            book1[s[i]] = book1.get(s[i],0) + 1;
+        
+        for i in range(Lt):
+            if(t[i] not in book1):
+                return False;
+            else:
+                book2[t[i]] = book2.get(t[i],0) + 1;
+            
+        return True if book1 == book2 else False;
+            
