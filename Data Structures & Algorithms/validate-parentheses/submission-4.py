86cb9f1dac7e851f332557fd6bba8c0ea847c280
Richard <152018239+RichardWASD@users.noreply.github.com>
2026-07-30 13:32:49 -0400

    Add: validate-parentheses - submission-4

---
+++ Data Structures & Algorithms/validate-parentheses/submission-4.py
@@ -0,0 +1,28 @@
+class Solution:
+    '''
+    U:
+        I: A string of bracket type chars 
+        O: Boolean 
+        C: Strict requirement
+        E: No Closing
+    I:
+    '''
+    def isValid(self, s: str) -> bool:
+        key  = {
+            '}' : '{',
+            ']' : '[',
+            ')' : '('
+        };
+        stack = [];
+        
+        for c in s:
+
+            if(c in key):
+
+                if(stack and stack[-1] == key[c] ): # TODO
+                    stack.pop();
+                else:
+                    return False;
+            else:
+                stack.append(c);
+        return True if not stack else False;
